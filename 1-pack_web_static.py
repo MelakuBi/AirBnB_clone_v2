603c17f2cff8259d8f9a80ce85ee54171b7342c5
MelakuBi <MelakuBirhanu@gmail.com>
2023-06-20 12:06:14 -0700

    $msg

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-""" Function that compress a folder """
-from datetime import datetime
+# 1. Compress before sending
 from fabric.api import local
+from datetime import datetime
 
 
 def do_pack():
-    """
-    must return the archive path if the archive has been correctly
-    generated. Otherwise, it should return None
-    """
+    """ generates a .tgz archive """
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    my_tar = "versions/web_static_{}.tgz".format(date)
     try:
-        local("mkdir -p versions")
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        rout = "versions/web_static_{}.tgz".format(date)
-        _gzip = local("tar -cvzf {} web_static".format(rout))
-        return rout
-    except Exception:
+        local("tar -czvf {} web_static".format(my_tar))
+        return my_tar
+    except:
         return None
