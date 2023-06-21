c14bbcbeaf41cc8f81b980c30ddc6495046b0c6c
MelakuBi <MelakuBirhanu@gmail.com>
2023-06-21 12:56:40 -0700

    $msg

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,88 +1,79 @@
-# Configures a web server for deployment of web_static.
-
-# Nginx configuration file
-$nginx_conf = "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By ${hostname};
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}"
-
-package { 'nginx':
-  ensure   => 'present',
-  provider => 'apt'
-} ->
-
-file { '/data':
-  ensure  => 'directory'
-} ->
-
-file { '/data/web_static':
-  ensure => 'directory'
-} ->
-
-file { '/data/web_static/releases':
-  ensure => 'directory'
-} ->
-
-file { '/data/web_static/releases/test':
-  ensure => 'directory'
-} ->
-
-file { '/data/web_static/shared':
-  ensure => 'directory'
-} ->
-
-file { '/data/web_static/releases/test/index.html':
-  ensure  => 'present',
-  content => "Holberton School Puppet\n"
-} ->
-
-file { '/data/web_static/current':
-  ensure => 'link',
-  target => '/data/web_static/releases/test'
-} ->
-
-exec { 'chown -R ubuntu:ubuntu /data/':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
+# 5. Puppet for setup - Redo the task #0 but by using Puppet:
+exec { "exec_1":
+  command => "sudo apt-get update -y",
+  path    => ["/usr/bin", "/bin"],
+  returns => [0,1],
+  before   => Exec["exec_2"]
 }
 
-file { '/var/www':
-  ensure => 'directory'
-} ->
+exec { "exec_2":
+  require => Exec["exec_1"],
+  command => "sudo apt-get install nginx -y",
+  path    => ["/usr/bin", "/bin"],
+  returns => [0,1],
+  before   => Exec["exec_3"]
+}
 
-file { '/var/www/html':
-  ensure => 'directory'
-} ->
+exec { "exec_3":
+  require => Exec["exec_2"],
+  command => "sudo mkdir -p /data/web_static/shared/",
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1],
+  before   => Exec['exec_4']
+}
 
-file { '/var/www/html/index.html':
-  ensure  => 'present',
-  content => "Holberton School Nginx\n"
-} ->
+exec { "exec_4":
+  require => Exec["exec_3"],
+  command => "sudo mkdir -p /data/web_static/releases/test/",
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1],
+  before   => Exec['exec_5']
+}
 
-file { '/var/www/html/404.html':
-  ensure  => 'present',
-  content => "Ceci n'est pas une page\n"
-} ->
+exec { "exec_5":
+  require => Exec["exec_4"],
+  command => "echo 'Testing Nginx configuration' | sudo tee -a /data/web_static/releases/test/index.html",
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1],
+  before   => Exec['exec_6']
+}
 
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'present',
-  content => $nginx_conf
-} ->
+exec { "exec_6":
+  require => Exec["exec_5"],
+  command => "sudo rm -rf /data/web_static/current/",
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1],
+  before   => Exec['exec_7']
+}
 
-exec { 'nginx restart':
-  path => '/etc/init.d/'
+exec { "exec_7":
+  require => Exec["exec_6"],
+  command => "sudo ln -fs /data/web_static/releases/test/ /data/web_static/current",
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1],
+  before   => Exec['exec_8']
+}
+
+exec { "exec_8":
+  require => Exec["exec_7"],
+  command => "sudo chown -R ubuntu:ubuntu /data/",
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1],
+  before   => Exec['exec_9']
+}
+
+exec { "exec_9":
+  require => Exec["exec_8"],
+  environment => ["upd=\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n"],
+  command => "sudo sed -i '38i $upd' /etc/nginx/sites-available/default",
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1],
+  before   => Exec['exec_10']
+}
+
+exec { 'exec_10':
+  require => Exec['exec_9'],
+  command => 'sudo service nginx restart',
+  path    => ['/usr/bin', '/bin', '/usr/sbin'],
+  returns => [0,1]
 }
