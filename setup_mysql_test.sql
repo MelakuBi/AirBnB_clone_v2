6e41f2b8c0df504fe7caac67d2425d931dc13dda
MelakuBi <MelakuBirhanu@gmail.com>
2023-06-24 08:57:14 -0700

    all done

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,12 +1,6 @@
--- creating test user and giving it permissions
+-- Creating User and givig it permissions for test user
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-GRANT USAGE ON *.*
-      TO 'hbnb_test'@'localhost'
-      IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.*
-      TO 'hbnb_test'@'localhost'
-      IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON performance_schema.*
-      TO 'hbnb_test'@'localhost'
-      IDENTIFIED BY 'hbnb_test_pwd';
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
